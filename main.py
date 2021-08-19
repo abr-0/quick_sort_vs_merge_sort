3bcd0e4308690017b79d84bb6b04f329d26078c4
abr-0 <10011000010@protonmail.com>
2021-08-19 15:01:56 +0545

    add merge sort

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import random, time
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
 # random function which returns list of random numbers
 def rand_gen(quantity):
@@ -30,25 +30,59 @@ def quick_sort(data, lb, ub):
         quick_sort(data, lb, p-1)
         quick_sort(data, p+1, ub)
 
+def merge_sort(data):
+    if len(data) > 1:
+        mid = len(data)//2
+        # slicing
+        lh = data[:mid]
+        rh = data[mid:]
+
+        merge_sort(lh)
+        merge_sort(rh)
+        i=j=k=0
+        while i < len(lh) and j < len(rh):
+            if lh[i] < rh[j]:
+                data[k]=lh[i]
+                i=i+1
+            else:
+                data[k]=rh[j]
+                j=j+1
+            k=k+1
+
+        while i < len(lh):
+            data[k]=lh[i]
+            i=i+1
+            k=k+1
+
+        while j < len(rh):
+            data[k]=rh[j]
+            j=j+1
+            k=k+1
+
 def main():
-    data = rand_gen(5)
-    quick_sort(data, 0, len(data)-1)
-    print(data)
+    x1 = [] # no. of elements to sort
+    y1 = [] # time for quick sort
+    y2 = [] # time for merge sort
+    for i in range(10, 1000, 10):
+        data1 = data2 = rand_gen(i)
+        x1.append(i)
+        # quick sort
+        start_t1 = time.time()
+        quick_sort(data1, 0, len(data1)-1)
+        end_t1 = time.time()
+        y1.append(end_t1-start_t1)
+        
+        # merge sort
+        start_t2 = time.time()
+        merge_sort(data2)
+        end_t2 = time.time()
+        y2.append(end_t2-start_t2)
+
+    # plotting graph
+    
 
 if __name__ == "__main__":
     main()
-
-
-# x1 = []
-# y1 = []
-# for i in range(0, 10000, 100):
-#     array1 = rand_gen(i)
-#     start_t1 = time.time()
-#     quick_sort(array1, 0, len(array1)-1)
-#     end_t1 = time.time()
-#     x1.append(i)
-#     y1.append(end_t1-start_t1)
-    
 
 # plt.plot(x1,y1)
 # print(y1)
