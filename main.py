ca3c92ae7b636f29bfc7aea11498183a47e2238b
abr-0 <10011000010@protonmail.com>
2021-08-19 13:33:24 +0545

    add quick sort

--- main.py
+++ main.py
@@ -1,40 +1,56 @@
 import random, time
+# import matplotlib.pyplot as plt
 
+# random function which returns list of random numbers
 def rand_gen(quantity):
-    start = time.time()
     l = []
     for i in range(quantity):
-        x = random.randint(0, 1000)
+        x = random.randint(0, quantity)
         l.append(x)
     return l
 
-start = time.time()
-def partition(array, lb, ub):
-    pivot = array[lb]
-    start = lb + 1
-    end = ub
+def partition(data, lb, ub):
+    p = data[lb]
+    i = lb + 1
+    j = ub
+    while i <= j:
+        while i <= j and data[i] <= p:
+            i += 1
+        while i <= j and data[j] >= p:
+            j -= 1
+        if i <= j:
+            data[i], data[j] =  data[j], data[i]
 
-    while True:
-        while start <= end and array[start] <= pivot:
-            start = start + 1
-        while start <= end and array[end] >= pivot:
-            end = end - 1
-        if start <= end:
-            array[start], array[end] = array[end], array[start]
-        else:
-            break
+    data[lb], data[j] = data[j], data[lb]
+    return j
 
-    array[lb], array[end] = array[end], array[lb]
-    return end
+def quick_sort(data, lb, ub):
+    if lb < ub:
+        p = partition(data, lb, ub)
+        quick_sort(data, lb, p-1)
+        quick_sort(data, p+1, ub)
 
-def quick_sort(array, lb, ub):
-    if lb >= ub:
-        return
+def main():
+    data = rand_gen(5)
+    quick_sort(data, 0, len(data)-1)
+    print(data)
 
-    p = partition(array, lb, ub)
-    quick_sort(array, lb, p-1)
-    quick_sort(array, p+1, ub)
+if __name__ == "__main__":
+    main()
 
-array1 = rand_gen(100000)
-quick_sort(array1, 0, len(array1)-1)
-print(f'Time taken for {len(array1)} numbers: {time.time()-start}')
+
+# x1 = []
+# y1 = []
+# for i in range(0, 10000, 100):
+#     array1 = rand_gen(i)
+#     start_t1 = time.time()
+#     quick_sort(array1, 0, len(array1)-1)
+#     end_t1 = time.time()
+#     x1.append(i)
+#     y1.append(end_t1-start_t1)
+    
+
+# plt.plot(x1,y1)
+# print(y1)
+# print(sum(y1))
+# plt.show()
