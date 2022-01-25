9999c29f65c4c06a5999f55c1a9bc91e0d467946
abr-0 <ashutoshmandal115@gmail.com>
2022-01-25 16:09:22 +0545

    refactor

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ def rand_gen(quantity):
         l.append(x)
     return l
 
+
 # partition funciton for quick sort
 def partition(data, lb, ub):
     p = data[lb]
@@ -20,70 +21,74 @@ def partition(data, lb, ub):
         while i <= j and data[j] >= p:
             j -= 1
         if i <= j:
-            data[i], data[j] =  data[j], data[i]
+            data[i], data[j] = data[j], data[i]
 
     data[lb], data[j] = data[j], data[lb]
     return j
 
+
 def quick_sort(data, lb, ub):
     if lb < ub:
         p = partition(data, lb, ub)
-        quick_sort(data, lb, p-1) # perform quick sort on left half
-        quick_sort(data, p+1, ub) # perform quick sort on right half
+        quick_sort(data, lb, p - 1)  # perform quick sort on left half
+        quick_sort(data, p + 1, ub)  # perform quick sort on right half
+
 
 def merge_sort(data):
     if len(data) > 1:
-        mid = len(data)//2
+        mid = len(data) // 2
         # slicing
         lh = data[:mid]
         rh = data[mid:]
 
         merge_sort(lh)
         merge_sort(rh)
-        i=j=k=0
+        i = j = k = 0
         while i < len(lh) and j < len(rh):
             if lh[i] < rh[j]:
-                data[k]=lh[i]
-                i=i+1
+                data[k] = lh[i]
+                i = i + 1
             else:
-                data[k]=rh[j]
-                j=j+1
-            k=k+1
+                data[k] = rh[j]
+                j = j + 1
+            k = k + 1
 
         while i < len(lh):
-            data[k]=lh[i]
-            i=i+1
-            k=k+1
+            data[k] = lh[i]
+            i = i + 1
+            k = k + 1
 
         while j < len(rh):
-            data[k]=rh[j]
-            j=j+1
-            k=k+1
+            data[k] = rh[j]
+            j = j + 1
+            k = k + 1
+
 
 def main():
-    x1 = [] # no. of elements to sort
-    y1 = [] # time for quick sort
-    y2 = [] # time for merge sort
+    x1 = []  # no. of elements to sort
+    y1 = []  # time for quick sort
+    y2 = []  # time for merge sort
     for i in range(1, 10000, 100):
         data1 = data2 = rand_gen(i)
         x1.append(i)
         # quick sort
         start_t1 = time.time()
-        quick_sort(data1, 0, len(data1)-1)
+        quick_sort(data1, 0, len(data1) - 1)
         end_t1 = time.time()
-        y1.append(end_t1-start_t1)
-        
+        y1.append(end_t1 - start_t1)
+
         # merge sort
         start_t2 = time.time()
         merge_sort(data2)
         end_t2 = time.time()
-        y2.append(end_t2-start_t2)
+        y2.append(end_t2 - start_t2)
 
     # plotting graph
-    plt.plot(x1,y1, label = "quick sort")
-    plt.plot(x1,y2, label = "merge sort")
+    plt.plot(x1, y1, label="quick sort")
+    plt.plot(x1, y2, label="merge sort")
     plt.legend()
     plt.show()
+
 
 if __name__ == "__main__":
     main()
